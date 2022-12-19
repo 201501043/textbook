2649d70472c843092eeb4b04e6f13ad5d026f6c4
201501043 <109889715+201501043@users.noreply.github.com>
2022-12-19 23:04:02 +0530

    update after long days

---
+++ src/components/home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import "./home.css"
+import Title from "./title"
+export default class home extends Component {
+  render() {
+    return (
+      <section className='intro'>
+        <Title/>
+        <div className='book-image-container'>
+          
+        </div>
+      </section>
+    )
+  }
+}
