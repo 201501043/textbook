ba8ac447fca76c3ca4309bb1790f0716b5392e31
201501043 <109889715+201501043@users.noreply.github.com>
2022-12-20 11:23:26 +0530

    color gradient

--- src/components/home.js
+++ src/components/home.js
@@ -1,13 +1,54 @@
 import React, { Component } from 'react'
 import "./home.css"
 import Title from "./title"
+import book1 from "./book1.jpg"
+import book2 from "./book2.jpg"
 export default class home extends Component {
+  constructor(props){
+    super(props)
+    this.state = {book:[book1,book2],
+                  current:0}
+  }
+
+  componentDidMount(){
+    setInterval(()=>this.changeRight(),7000);
+  }
+
+  changeRight(){
+    var index = this.state.current;
+    index = index + 1
+    if(index%2 == 0){
+      index = 0
+    }
+    else{
+      index = 1 
+    }
+    this.setState({current:index})
+  }
+
+  changeLeft(){
+    var index = this.state.current;
+    index = index - 1
+    if(index == -1){
+      index = this.state.book.length - 1
+      console.log(index)
+    }
+    this.setState({current:index})
+  }
+
   render() {
     return (
       <section className='intro'>
         <Title/>
         <div className='book-image-container'>
+          <div>
+            <button className='book-image-left-button caro-nav' onClick={()=>this.changeLeft()}>&lt;</button>
+          </div>
           
+          <img src={this.state.book[this.state.current]} className='book'/>
+          <div>
+            <button className='book-image-right-button caro-nav' onClick={()=>this.changeRight()}>&gt;</button>
+          </div>
         </div>
       </section>
     )
