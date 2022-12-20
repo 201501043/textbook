a316331074167c73113c321a67ceece57ae60ba9
201501043 <109889715+201501043@users.noreply.github.com>
2022-12-20 22:08:29 +0530

    books

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
-import Home from'./components/home';
-
+import Home from './components/home';
+import Body from './components/body';
 function App() {
   return (
     <div className="App">
         <Home/>
+        <Body/>
     </div>
   );
 }
--- src/components/body.js
+++ src/components/body.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react'
-
+import "./body.css"
+import book1 from "./cyberbook1.png"
+import book2 from "./cyberbook2.png"
 export default class body extends Component {
   render() {
     return (
-      <div>
-        
-      </div>
+      <section className='body'>
+        <div className='body-heading'>
+          <h1>S E M E S T E R - &#8548;</h1>
+        </div>
+        <div className='body-card-view'>
+          <h1>CYBER SECURITY</h1>
+          <div className='book-container'>
+            <a href="./pdf/Cyber Security Text Book 1.pdf"><img src={book1} className="books"/></a>
+            <a href=""><img src={book2} className="books"/></a>
+          </div>
+        </div>
+      </section>
     )
   }
 }
